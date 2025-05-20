8d56c03b19d71bf8b312c8491a293eaa7870af72
JayShende <shendejay379@gmail.com>
2025-05-20 14:50:21 +0530

    add the send button for what with conditionally rendered tooltip

--- components/send-message.tsx
+++ components/send-message.tsx
@@ -28,24 +28,43 @@ const SendMessage = () => {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const [msgLength, setMsgLength] = useState(0);
   const [msg, setMsg] = useState("");
+  const [btnClass, setBtnClass] = useState("text-neutral-400");
+  const [showTooltip, setshowTooltip] = useState(false);
 
   const autoResize = () => {
     const textarea = textareaRef.current;
     if (textarea) {
-      textarea.style.height = "auto"; // Reset height
-      textarea.style.height = `${textarea.scrollHeight}px`; // Set to scroll height
+      textarea.style.height = "auto";
+      textarea.style.height = `${textarea.scrollHeight}px`;
     }
   };
 
   useEffect(() => {
     const numofChar = msg.length;
+    if (numofChar != 0) {
+      setBtnClass("bg-black text-white");
+      setshowTooltip(true);
+    }
+    if (numofChar == 0) {
+      setBtnClass("text-neutral-400");
+      setshowTooltip(false);
+    }
     setMsgLength(numofChar);
   }, [msg]);
+
+  const sendButton = (
+    <button
+      className={`p-2 rounded-2xl w-24 flex items-center justify-center gap-x-2 ${btnClass}`}
+    >
+      <span className="font-medium">Send</span>
+      <FaChevronDown className="text-sm" />
+    </button>
+  );
 
   return (
     <TooltipProvider>
       <Tooltip>
-        <div className="flex gap-x-2 p-3 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] w-full h-fit my-1 rounded-lg flex-col justify-between ">
+        <div className="flex gap-x-2 p-3 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] w-full h-auto  my-1 rounded-lg flex-col justify-between ">
           <div className="flex gap-x-2 items-center hover:text-red-500 cursor-pointer w-fit">
             <BsChatLeftTextFill />
             <span className={cn("font-medium ", interFont.className)}>
@@ -53,10 +72,13 @@ const SendMessage = () => {
             </span>
             <FaChevronDown className="text-sm" />
           </div>
-          <div className="my-4 text-wrap">
+          <div className="mt-1 text-wrap">
             <textarea
               placeholder="Use CtrlK for Shortcuts"
-              className={cn("w-full border-0 outline-0 py-2 resize-none overflow-y-auto max-h-[200px] break-words p-0 font-medium",RobotoFont.className)}
+              className={cn(
+                "w-full border-0 outline-0 py-0 resize-none overflow-y-auto max-h-[200px]  break-words p-0 font-medium ",
+                RobotoFont.className
+              )}
               rows={2}
               ref={textareaRef}
               onInput={autoResize}
@@ -70,7 +92,7 @@ const SendMessage = () => {
               </span>
             )}
           </div>
-          <div>
+          <div className="flex justify-between">
             <div className="flex gap-x-4">
               <Tooltip>
                 <TooltipTrigger>
@@ -98,6 +120,20 @@ const SendMessage = () => {
                   <p>Insert Emoji</p>
                 </TooltipContent>
               </Tooltip>
+            </div>
+            <div>
+              <TooltipProvider>
+                {showTooltip ? (
+                  <Tooltip>
+                    <TooltipTrigger asChild>{sendButton}</TooltipTrigger>
+                    <TooltipContent>
+                      <p>Send Message</p>
+                    </TooltipContent>
+                  </Tooltip>
+                ) : (
+                  sendButton
+                )}
+              </TooltipProvider>
             </div>
           </div>
         </div>
