1662596ff65dceb7e5574d93d6f5760cb41f9341
JayShende <shendejay379@gmail.com>
2025-05-20 14:12:09 +0530

    add the text area input with various modification like auto resize and scrollbar and charcter counters

--- components/send-message.tsx
+++ components/send-message.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { BsFillLightningChargeFill } from "react-icons/bs";
-import { Separator } from "./ui/separator";
 import { FaBookmark } from "react-icons/fa";
 import { FaFaceSmile } from "react-icons/fa6";
 import { BsChatLeftTextFill } from "react-icons/bs";
 import { FaChevronDown } from "react-icons/fa";
-import { Inter } from "next/font/google";
+import { Inter, Roboto } from "next/font/google";
 import { cn } from "@/lib/utils";
 import {
   Tooltip,
@@ -13,16 +12,22 @@ import {
   TooltipTrigger,
   TooltipContent,
 } from "./ui/tooltip";
-import { Input } from "./ui/input";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const interFont = Inter({
   subsets: ["latin"],
   weight: ["400", "600", "700"],
 });
 
+const RobotoFont = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
 const SendMessage = () => {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [msgLength, setMsgLength] = useState(0);
+  const [msg, setMsg] = useState("");
 
   const autoResize = () => {
     const textarea = textareaRef.current;
@@ -31,6 +36,12 @@ const SendMessage = () => {
       textarea.style.height = `${textarea.scrollHeight}px`; // Set to scroll height
     }
   };
+
+  useEffect(() => {
+    const numofChar = msg.length;
+    setMsgLength(numofChar);
+  }, [msg]);
+
   return (
     <TooltipProvider>
       <Tooltip>
@@ -45,11 +56,19 @@ const SendMessage = () => {
           <div className="my-4 text-wrap">
             <textarea
               placeholder="Use CtrlK for Shortcuts"
-             className="w-full border-0 outline-0 py-2 resize-none overflow-y-auto max-h-[200px] break-words"
+              className={cn("w-full border-0 outline-0 py-2 resize-none overflow-y-auto max-h-[200px] break-words p-0 font-medium",RobotoFont.className)}
               rows={2}
               ref={textareaRef}
               onInput={autoResize}
+              onChange={(e) => setMsg(e.target.value)}
             />
+            {msgLength == 0 ? (
+              <span className=" inline-block opacity-0">Char USed</span>
+            ) : (
+              <span className="text-neutral-400 font-medium text-sm">
+                {msgLength} Characters Used
+              </span>
+            )}
           </div>
           <div>
             <div className="flex gap-x-4">
